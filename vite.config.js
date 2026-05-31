f6ac3d84ca5ad89ac938abbcd8865f4f2b9efec7
aesteves3 <armando.esteves@gmail.com>
2026-05-31 14:10:54 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
-import prerender from 'vite-plugin-prerender'
-
-const Renderer = prerender.PuppeteerRenderer
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -12,31 +9,7 @@ export default defineConfig(({ mode }) => {
   return {
     base,
     logLevel: 'error',
-    plugins: [
-      react(),
-      prerender({
-        staticDir: path.join(__dirname, 'dist'),
-        routes: [
-          '/',
-          '/curso/arrais',
-          '/curso/mestre',
-          '/curso/capitao',
-          '/pratica/arrais',
-          '/pratica/vela',
-          '/internacional/nauticed',
-          '/internacional/asa',
-          '/internacional/issa',
-          '/referencias',
-          '/livros',
-          '/comandantes',
-        ],
-        renderer: new Renderer({
-          headless: true,
-          renderAfterDocumentEvent: 'render-event',
-          renderAfterTime: 3000,
-        }),
-      }),
-    ],
+    plugins: [react()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
