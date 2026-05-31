62eba6059d5778e48aa301c54a632c896b8c99ab
aesteves3 <armando.esteves@gmail.com>
2026-05-31 13:31:11 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
+import prerender from 'vite-plugin-prerender'
+
+const Renderer = prerender.PuppeteerRenderer
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -9,7 +12,31 @@ export default defineConfig(({ mode }) => {
   return {
     base,
     logLevel: 'error',
-    plugins: [react()],
+    plugins: [
+      react(),
+      prerender({
+        staticDir: path.join(__dirname, 'dist'),
+        routes: [
+          '/',
+          '/curso/arrais',
+          '/curso/mestre',
+          '/curso/capitao',
+          '/pratica/arrais',
+          '/pratica/vela',
+          '/internacional/nauticed',
+          '/internacional/asa',
+          '/internacional/issa',
+          '/referencias',
+          '/livros',
+          '/comandantes',
+        ],
+        renderer: new Renderer({
+          headless: true,
+          renderAfterDocumentEvent: 'render-event',
+          renderAfterTime: 3000,
+        }),
+      }),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
